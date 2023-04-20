6fc211d868a281415d076397b010361a8f5a31c0
sergio dellepiane espinoza <sdellepiane@genium.io>
2023-04-20 16:31:16 -0500

    Feat | Implement retrofit and get popular movies api | Sergio Dellepiane

---
+++ app/app/src/main/java/com/app/instaleapapp/data/model/PopularMoviesResponse.kt
@@ -0,0 +1,26 @@
+package com.app.instaleapapp.data.model
+
+data class PopularMoviesResponse(
+	val page: Int? = null,
+	val totalPages: Int? = null,
+	val results: List<ResultsItem?>? = null,
+	val totalResults: Int? = null
+)
+
+data class ResultsItem(
+	val overview: String? = null,
+	val originalLanguage: String? = null,
+	val originalTitle: String? = null,
+	val video: Boolean? = null,
+	val title: String? = null,
+	val genreIds: List<Int?>? = null,
+	val posterPath: String? = null,
+	val backdropPath: String? = null,
+	val releaseDate: String? = null,
+	val popularity: Any? = null,
+	val voteAverage: Any? = null,
+	val id: Int? = null,
+	val adult: Boolean? = null,
+	val voteCount: Int? = null
+)
+
---
+++ app/app/src/main/java/com/app/instaleapapp/data/remote/Api.kt
@@ -0,0 +1,10 @@
+package com.app.instaleapapp.data.remote
+
+import com.app.instaleapapp.data.model.PopularMoviesResponse
+import retrofit2.http.GET
+
+interface Api {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(): PopularMoviesResponse
+}
